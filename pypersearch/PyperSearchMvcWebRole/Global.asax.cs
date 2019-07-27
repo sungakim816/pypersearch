9bfeeeb19dbe48154b50ffdac640701c1cb299f2
Kim Sunga <sungakim816@gmail.com>
2019-07-27 13:02:39 +0800

    Added pagecount.csv, and starting working on hybrid trie data structure

--- pypersearch/PyperSearchMvcWebRole/Global.asax.cs
+++ pypersearch/PyperSearchMvcWebRole/Global.asax.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -16,6 +19,27 @@ namespace PyperSearchMvcWebRole
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            InitializeTrie();
+        }
+
+        private void InitializeTrie()
+        {
+            Dictionary<string, short> suggestions = new Dictionary<string, short>();
+            string filePath = Server.MapPath("~/App_Data/pagecount.csv");
+            FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            StreamReader streamReader = new StreamReader(fs, Encoding.UTF8);
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                string[] line_arr = line.Split(':');
+                string title = line_arr.FirstOrDefault();
+                short count = Convert.ToInt16(line.LastOrDefault());
+                suggestions.Add(title, count);
+            }
+            fs.Close();
+            HttpRuntime.Cache.Insert("suggestions", suggestions, null,
+                Cache.NoAbsoluteExpiration, Cache.NoSlidingExpiration, CacheItemPriority.NotRemovable, null);
+            
         }
     }
 }
